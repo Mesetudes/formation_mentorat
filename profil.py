7931fa8480e70d991e0a6be9e5d88c51407d219d
Serigne Thierno BOUSSO <mesetudes17660@gmail.com>
2026-09-12 04:48:33 +0100

    Ajout du fichier profil

---
+++ profil.py
@@ -0,0 +1,9 @@
+str 
+prenom = "Serugne Thierno BOUSSO"
+int 
+age = 20
+float 
+moyenne= 15.5
+bool
+estudiant = True
+print(f"Bonjour, je m'appelle {prenom}, j'ai {age} ans, ma moyenne est de {moyenne} et je suis étudiant: {estudiant}")
